b7f78243a49fc9310cd15add8233356b9838b7cc
Maximilian Becker <mbecker@databay.de>
2019-05-20 12:03:57 +0200

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -8,7 +8,7 @@ $version = '1.5.3';
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = '4.2.0';
-$ilias_max_version = '5.3.999';
+$ilias_max_version = '5.4.999';
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = 'Michael Jansen';

b456a68cb204fbd415f28fde88dccf4952d4214d
Maximilian Becker <mbecker@databay.de>
2024-12-05 08:58:43 +0100

    Mantis 43083

--- classes/class.ilObjTestOverviewGUI.php
+++ classes/class.ilObjTestOverviewGUI.php
@@ -270,7 +270,7 @@ class ilObjTestOverviewGUI extends ilObjectPluginGUI implements ilDesktopItemHan
             $this->selectTests();
             return;
         }
-        $this->tpl->setOnScreenMessage(ilGlobalTemplateInterface::MESSAGE_TYPE_SUCCESS, $this->lng->txt('tests_updated_success'), true);
+        $this->tpl->setOnScreenMessage(ilGlobalTemplateInterface::MESSAGE_TYPE_SUCCESS, $this->lng->txt('rep_robj_xtov_tests_updated_success'), true);
         $this->ctrl->redirect($this, 'editSettings');
 
         $this->editSettings();
@@ -330,7 +330,7 @@ class ilObjTestOverviewGUI extends ilObjectPluginGUI implements ilDesktopItemHan
             return;
         }
 
-        $this->tpl->setOnScreenMessage(ilGlobalTemplateInterface::MESSAGE_TYPE_SUCCESS, $this->txt('tests_updated_success'), true);
+        $this->tpl->setOnScreenMessage(ilGlobalTemplateInterface::MESSAGE_TYPE_SUCCESS, $this->txt('rep_robj_xtov_tests_updated_success'), true);
         $this->ctrl->redirect($this, 'editSettings');
 
         $this->editSettings();
